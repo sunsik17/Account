1410085a880c7bda76399a685554ec114ee9f3b0
NSS <nssp0410@naver.com>
2023-01-30 02:52:12 +0900

    account file commit

---
+++ src/main/java/com/example/account/domain/AccountUser.java
@@ -0,0 +1,16 @@
+package com.example.account.domain;
+
+
+import lombok.*;
+
+import javax.persistence.Entity;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class AccountUser extends BaseEntity {
+    private String name;
+}
